088a5432c37d4cbbb2329da8781f3a75a727c723
rushil gupta <rushilcodes@gmail.com>
2025-02-10 09:22:01 +0530

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  tabWidth: 2,
+  printWidth: 100,
+  arrowParens: "always"
+};
